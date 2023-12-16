94c832ddc830559d7be1fb24718705ae0b58c3b4
quân huỳnh <huynhkiengquan@gmail.com>
2023-12-16 22:51:10 +0700

    Add Daily Update.

--- src/routes/gmail_account.ts
+++ src/routes/gmail_account.ts
@@ -1,6 +1,6 @@
 const username='huynhkiengquan@gmail.com';
 const password='zurbreuybglqqqmh';
-const registerUrl='http://localhost:3000/register';
-const loginUrl='http://localhost:3000/login';
-const registerServerUrl='http://localhost:9000/register';
+const registerUrl='https://sudoku-master-9ytq.onrender.com/register';
+const loginUrl='https://sudoku-master-9ytq.onrender.com/login';
+const registerServerUrl='https://sudoku-server-2sz5.onrender.com/register';
 export {username,password,registerUrl,loginUrl,registerServerUrl};
